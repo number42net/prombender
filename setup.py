ba68855deb4eb415c5a31acdbf8fefd2bb001bcd
number42net <github@number42.net>
2021-11-18 09:45:47 +0100

    Install fixes

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='prombender',
     version='1.0',
-    package_dir={"": "src"},
-    packages=find_packages(),
+    packages=['prombender'],
     entry_points = {
         'console_scripts': ['prombender=prombender.command_line:main'],
     }
